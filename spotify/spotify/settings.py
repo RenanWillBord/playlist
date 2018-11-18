8c7cb61a9fcf1c919cfd49ada73701682fd2b268
Renan <renan.wbordignon@gmail.com>
2018-11-18 00:39:33 -0200

    Renan
    .

--- spotify/spotify/settings.py
+++ spotify/spotify/settings.py
@@ -153,6 +153,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "images")
